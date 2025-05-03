ca9b1cdb2999e2fc49554256be5890af37f988a8
AaronCasildo <aacasildor@gmail.com>
2025-05-02 20:40:50 -0700

    Mejora visual en la interfaz de AddBarcodes y Main

--- LectorBarcodes/AddBarcodes.Designer.cs
+++ LectorBarcodes/AddBarcodes.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddBarcodes));
             this.cmd_back = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.cbox_camera = new System.Windows.Forms.ComboBox();
@@ -44,121 +45,116 @@
             // 
             // cmd_back
             // 
-            this.cmd_back.Location = new System.Drawing.Point(32, 499);
-            this.cmd_back.Margin = new System.Windows.Forms.Padding(4);
+            this.cmd_back.Location = new System.Drawing.Point(24, 490);
             this.cmd_back.Name = "cmd_back";
-            this.cmd_back.Size = new System.Drawing.Size(176, 28);
-            this.cmd_back.TabIndex = 0;
-            this.cmd_back.Text = "Regresar al menú...";
-            this.cmd_back.UseVisualStyleBackColor = true;
+            this.cmd_back.Size = new System.Drawing.Size(192, 30);
+            this.cmd_back.TabIndex = 1;
+            this.cmd_back.Text = "Regresar";
             this.cmd_back.Click += new System.EventHandler(this.cmd_back_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(44, 31);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(24, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(129, 16);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Seleccione Cámara:";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.Size = new System.Drawing.Size(144, 19);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Seleccionar cámara:";
             // 
             // cbox_camera
             // 
-            this.cbox_camera.FormattingEnabled = true;
-            this.cbox_camera.Location = new System.Drawing.Point(219, 27);
-            this.cbox_camera.Margin = new System.Windows.Forms.Padding(4);
+            this.cbox_camera.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbox_camera.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.cbox_camera.Location = new System.Drawing.Point(190, 18);
             this.cbox_camera.Name = "cbox_camera";
-            this.cbox_camera.Size = new System.Drawing.Size(216, 24);
-            this.cbox_camera.TabIndex = 2;
+            this.cbox_camera.Size = new System.Drawing.Size(234, 23);
+            this.cbox_camera.TabIndex = 9;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(32, 70);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(24, 60);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(533, 400);
-            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.Size = new System.Drawing.Size(400, 300);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 8;
             this.pictureBox1.TabStop = false;
             // 
             // cmd_readcode
             // 
-            this.cmd_readcode.Location = new System.Drawing.Point(230, 499);
-            this.cmd_readcode.Margin = new System.Windows.Forms.Padding(4);
+            this.cmd_readcode.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.cmd_readcode.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.cmd_readcode.ForeColor = System.Drawing.Color.White;
+            this.cmd_readcode.Location = new System.Drawing.Point(24, 375);
             this.cmd_readcode.Name = "cmd_readcode";
-            this.cmd_readcode.Size = new System.Drawing.Size(335, 67);
-            this.cmd_readcode.TabIndex = 4;
+            this.cmd_readcode.Size = new System.Drawing.Size(400, 40);
+            this.cmd_readcode.TabIndex = 7;
             this.cmd_readcode.Text = "Leer Código (C)";
-            this.cmd_readcode.UseVisualStyleBackColor = true;
+            this.cmd_readcode.UseVisualStyleBackColor = false;
             this.cmd_readcode.Click += new System.EventHandler(this.cmd_readcode_Click);
             // 
             // txt_resultado
             // 
-            this.txt_resultado.Location = new System.Drawing.Point(32, 574);
-            this.txt_resultado.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_resultado.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txt_resultado.Location = new System.Drawing.Point(24, 425);
             this.txt_resultado.Name = "txt_resultado";
-            this.txt_resultado.Size = new System.Drawing.Size(533, 22);
-            this.txt_resultado.TabIndex = 5;
+            this.txt_resultado.Size = new System.Drawing.Size(400, 25);
+            this.txt_resultado.TabIndex = 6;
             // 
             // lbl_status
             // 
             this.lbl_status.AutoSize = true;
-            this.lbl_status.Location = new System.Drawing.Point(29, 613);
-            this.lbl_status.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_status.Location = new System.Drawing.Point(24, 455);
             this.lbl_status.Name = "lbl_status";
-            this.lbl_status.Size = new System.Drawing.Size(0, 16);
-            this.lbl_status.TabIndex = 6;
+            this.lbl_status.Size = new System.Drawing.Size(0, 15);
+            this.lbl_status.TabIndex = 5;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(645, 30);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(446, 60);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(194, 16);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "Códigos de barra escaneados:";
+            this.label2.Size = new System.Drawing.Size(149, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Códigos escaneados:";
             // 
             // lbl_codehistory
             // 
             this.lbl_codehistory.AutoSize = true;
-            this.lbl_codehistory.Location = new System.Drawing.Point(645, 70);
-            this.lbl_codehistory.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_codehistory.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.lbl_codehistory.Location = new System.Drawing.Point(446, 90);
             this.lbl_codehistory.Name = "lbl_codehistory";
-            this.lbl_codehistory.Size = new System.Drawing.Size(0, 16);
-            this.lbl_codehistory.TabIndex = 8;
+            this.lbl_codehistory.Size = new System.Drawing.Size(0, 15);
+            this.lbl_codehistory.TabIndex = 3;
             // 
             // cmd_exit
             // 
-            this.cmd_exit.Location = new System.Drawing.Point(32, 538);
-            this.cmd_exit.Margin = new System.Windows.Forms.Padding(4);
+            this.cmd_exit.Location = new System.Drawing.Point(232, 490);
             this.cmd_exit.Name = "cmd_exit";
-            this.cmd_exit.Size = new System.Drawing.Size(176, 28);
-            this.cmd_exit.TabIndex = 9;
-            this.cmd_exit.Text = "Salir...";
-            this.cmd_exit.UseVisualStyleBackColor = true;
+            this.cmd_exit.Size = new System.Drawing.Size(192, 30);
+            this.cmd_exit.TabIndex = 0;
+            this.cmd_exit.Text = "Salir";
             this.cmd_exit.Click += new System.EventHandler(this.cmd_exit_Click);
             // 
             // cmd_clean
             // 
-            this.cmd_clean.Location = new System.Drawing.Point(648, 607);
-            this.cmd_clean.Margin = new System.Windows.Forms.Padding(4);
+            this.cmd_clean.Location = new System.Drawing.Point(449, 490);
             this.cmd_clean.Name = "cmd_clean";
-            this.cmd_clean.Size = new System.Drawing.Size(176, 28);
-            this.cmd_clean.TabIndex = 10;
-            this.cmd_clean.Text = "Limpiar...";
-            this.cmd_clean.UseVisualStyleBackColor = true;
+            this.cmd_clean.Size = new System.Drawing.Size(132, 30);
+            this.cmd_clean.TabIndex = 2;
+            this.cmd_clean.Text = "Limpiar";
             this.cmd_clean.Click += new System.EventHandler(this.button1_Click);
             // 
             // AddBarcodes
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(918, 652);
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(624, 540);
             this.ControlBox = false;
-            this.Controls.Add(this.cmd_clean);
             this.Controls.Add(this.cmd_exit);
+            this.Controls.Add(this.cmd_back);
+            this.Controls.Add(this.cmd_clean);
             this.Controls.Add(this.lbl_codehistory);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.lbl_status);
@@ -167,10 +163,14 @@
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.cbox_camera);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.cmd_back);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(640, 579);
+            this.MinimumSize = new System.Drawing.Size(640, 579);
             this.Name = "AddBarcodes";
-            this.Text = "AddBarcodes";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lector de Códigos de Barras";
+            this.TopMost = true;
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
--- LectorBarcodes/Form1.Designer.cs
+++ LectorBarcodes/Form1.Designer.cs
@@ -2,15 +2,11 @@
 {
     partial class Main
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button cmd_add;
+        private System.Windows.Forms.Button cmd_exit;
+        private System.Windows.Forms.Label lbl_title;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,58 +16,73 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.cmd_add = new System.Windows.Forms.Button();
-            this.cmd_read = new System.Windows.Forms.Button();
+            this.cmd_exit = new System.Windows.Forms.Button();
+            this.lbl_title = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // cmd_add
             // 
-            this.cmd_add.Location = new System.Drawing.Point(163, 289);
-            this.cmd_add.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmd_add.BackColor = System.Drawing.Color.White;
+            this.cmd_add.FlatAppearance.BorderColor = System.Drawing.Color.LightGray;
+            this.cmd_add.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmd_add.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.cmd_add.Location = new System.Drawing.Point(82, 71);
             this.cmd_add.Name = "cmd_add";
-            this.cmd_add.Size = new System.Drawing.Size(264, 28);
-            this.cmd_add.TabIndex = 0;
-            this.cmd_add.Text = "Agregar Códigos de Barras";
-            this.cmd_add.UseVisualStyleBackColor = true;
+            this.cmd_add.Size = new System.Drawing.Size(200, 40);
+            this.cmd_add.TabIndex = 1;
+            this.cmd_add.Text = "Leer Códigos de Barras";
+            this.cmd_add.UseVisualStyleBackColor = false;
             this.cmd_add.Click += new System.EventHandler(this.cmd_add_Click);
             // 
-            // cmd_read
+            // cmd_exit
             // 
-            this.cmd_read.Location = new System.Drawing.Point(163, 374);
-            this.cmd_read.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.cmd_read.Name = "cmd_read";
-            this.cmd_read.Size = new System.Drawing.Size(264, 28);
-            this.cmd_read.TabIndex = 1;
-            this.cmd_read.Text = "Salir...";
-            this.cmd_read.UseVisualStyleBackColor = true;
-            this.cmd_read.Click += new System.EventHandler(this.cmd_read_Click);
+            this.cmd_exit.BackColor = System.Drawing.Color.White;
+            this.cmd_exit.FlatAppearance.BorderColor = System.Drawing.Color.LightGray;
+            this.cmd_exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmd_exit.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.cmd_exit.Location = new System.Drawing.Point(82, 131);
+            this.cmd_exit.Name = "cmd_exit";
+            this.cmd_exit.Size = new System.Drawing.Size(200, 40);
+            this.cmd_exit.TabIndex = 2;
+            this.cmd_exit.Text = "Salir";
+            this.cmd_exit.UseVisualStyleBackColor = false;
+            this.cmd_exit.Click += new System.EventHandler(this.cmd_read_Click);
+            // 
+            // lbl_title
+            // 
+            this.lbl_title.AutoSize = true;
+            this.lbl_title.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.lbl_title.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.lbl_title.Location = new System.Drawing.Point(52, 31);
+            this.lbl_title.Name = "lbl_title";
+            this.lbl_title.Size = new System.Drawing.Size(261, 25);
+            this.lbl_title.TabIndex = 0;
+            this.lbl_title.Text = "Lector de Códigos de Barras";
+            this.lbl_title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(593, 635);
-            this.Controls.Add(this.cmd_read);
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(366, 201);
+            this.Controls.Add(this.lbl_title);
             this.Controls.Add(this.cmd_add);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Controls.Add(this.cmd_exit);
+            this.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(382, 240);
+            this.MinimumSize = new System.Drawing.Size(382, 240);
             this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Lector de Códigos de Barras";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
-
-        #endregion
-
-        private System.Windows.Forms.Button cmd_add;
-        private System.Windows.Forms.Button cmd_read;
     }
 }
-
