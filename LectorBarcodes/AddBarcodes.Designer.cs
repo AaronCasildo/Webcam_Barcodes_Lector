a6610f339ba652687345e970d4379a0c3c0507cc
AaronCasildo <aacasildor@gmail.com>
2025-05-06 22:34:30 -0700

    Agregamos análisis sintáctico, léxico y semántico.

--- LectorBarcodes/AddBarcodes.Designer.cs
+++ LectorBarcodes/AddBarcodes.Designer.cs
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddBarcodes));
             this.cmd_back = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
@@ -40,7 +42,18 @@
             this.lbl_codehistory = new System.Windows.Forms.Label();
             this.cmd_exit = new System.Windows.Forms.Button();
             this.cmd_clean = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dgv_lexico = new System.Windows.Forms.DataGridView();
+            this.lbl_tabladetokens = new System.Windows.Forms.Label();
+            this.lbl_validación = new System.Windows.Forms.Label();
+            this.dgv_sintactico = new System.Windows.Forms.DataGridView();
+            this.lbl_estructura = new System.Windows.Forms.Label();
+            this.lbl_elementos = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_lexico)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_sintactico)).BeginInit();
             this.SuspendLayout();
             // 
             // cmd_back
@@ -105,6 +118,7 @@
             // lbl_status
             // 
             this.lbl_status.AutoSize = true;
+            this.lbl_status.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_status.Location = new System.Drawing.Point(24, 455);
             this.lbl_status.Name = "lbl_status";
             this.lbl_status.Size = new System.Drawing.Size(0, 15);
@@ -142,16 +156,130 @@
             // 
             this.cmd_clean.Location = new System.Drawing.Point(449, 490);
             this.cmd_clean.Name = "cmd_clean";
-            this.cmd_clean.Size = new System.Drawing.Size(132, 30);
+            this.cmd_clean.Size = new System.Drawing.Size(146, 30);
             this.cmd_clean.TabIndex = 2;
             this.cmd_clean.Text = "Limpiar";
             this.cmd_clean.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(658, 412);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(137, 19);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Análisis Semántico:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(658, 214);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(132, 19);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Análisis Sintáctico:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(658, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 19);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Análisis Léxico:";
+            // 
+            // dgv_lexico
+            // 
+            this.dgv_lexico.AllowUserToAddRows = false;
+            this.dgv_lexico.AllowUserToDeleteRows = false;
+            this.dgv_lexico.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgv_lexico.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_lexico.Location = new System.Drawing.Point(662, 70);
+            this.dgv_lexico.Name = "dgv_lexico";
+            this.dgv_lexico.ReadOnly = true;
+            this.dgv_lexico.RowHeadersVisible = false;
+            this.dgv_lexico.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToFirstHeader;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dgv_lexico.RowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgv_lexico.Size = new System.Drawing.Size(597, 72);
+            this.dgv_lexico.TabIndex = 17;
+            // 
+            // lbl_tabladetokens
+            // 
+            this.lbl_tabladetokens.AutoSize = true;
+            this.lbl_tabladetokens.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_tabladetokens.Location = new System.Drawing.Point(687, 50);
+            this.lbl_tabladetokens.Name = "lbl_tabladetokens";
+            this.lbl_tabladetokens.Size = new System.Drawing.Size(103, 17);
+            this.lbl_tabladetokens.TabIndex = 18;
+            this.lbl_tabladetokens.Text = "Tabla de Tokens";
+            // 
+            // lbl_validación
+            // 
+            this.lbl_validación.AutoSize = true;
+            this.lbl_validación.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_validación.ForeColor = System.Drawing.Color.Blue;
+            this.lbl_validación.Location = new System.Drawing.Point(687, 157);
+            this.lbl_validación.Name = "lbl_validación";
+            this.lbl_validación.Size = new System.Drawing.Size(240, 34);
+            this.lbl_validación.TabIndex = 19;
+            this.lbl_validación.Text = "Longitud válida: Sí (13 dígitos)\r\nTodos los caracteres son numéricos: ✔️";
+            // 
+            // dgv_sintactico
+            // 
+            this.dgv_sintactico.AllowUserToAddRows = false;
+            this.dgv_sintactico.AllowUserToDeleteRows = false;
+            this.dgv_sintactico.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgv_sintactico.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_sintactico.Location = new System.Drawing.Point(662, 249);
+            this.dgv_sintactico.Name = "dgv_sintactico";
+            this.dgv_sintactico.ReadOnly = true;
+            this.dgv_sintactico.RowHeadersVisible = false;
+            this.dgv_sintactico.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToFirstHeader;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dgv_sintactico.RowsDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgv_sintactico.Size = new System.Drawing.Size(316, 136);
+            this.dgv_sintactico.TabIndex = 20;
+            // 
+            // lbl_estructura
+            // 
+            this.lbl_estructura.AutoSize = true;
+            this.lbl_estructura.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_estructura.ForeColor = System.Drawing.Color.Blue;
+            this.lbl_estructura.Location = new System.Drawing.Point(687, 478);
+            this.lbl_estructura.Name = "lbl_estructura";
+            this.lbl_estructura.Size = new System.Drawing.Size(30, 17);
+            this.lbl_estructura.TabIndex = 21;
+            this.lbl_estructura.Text = "test";
+            // 
+            // lbl_elementos
+            // 
+            this.lbl_elementos.AutoSize = true;
+            this.lbl_elementos.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_elementos.ForeColor = System.Drawing.Color.Blue;
+            this.lbl_elementos.Location = new System.Drawing.Point(687, 451);
+            this.lbl_elementos.Name = "lbl_elementos";
+            this.lbl_elementos.Size = new System.Drawing.Size(30, 17);
+            this.lbl_elementos.TabIndex = 22;
+            this.lbl_elementos.Text = "test";
+            // 
             // AddBarcodes
             // 
             this.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.ClientSize = new System.Drawing.Size(624, 540);
+            this.ClientSize = new System.Drawing.Size(1271, 540);
             this.ControlBox = false;
+            this.Controls.Add(this.lbl_elementos);
+            this.Controls.Add(this.lbl_estructura);
+            this.Controls.Add(this.dgv_sintactico);
+            this.Controls.Add(this.lbl_validación);
+            this.Controls.Add(this.lbl_tabladetokens);
+            this.Controls.Add(this.dgv_lexico);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.cmd_exit);
             this.Controls.Add(this.cmd_back);
             this.Controls.Add(this.cmd_clean);
@@ -165,13 +293,13 @@
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximumSize = new System.Drawing.Size(640, 579);
-            this.MinimumSize = new System.Drawing.Size(640, 579);
             this.Name = "AddBarcodes";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Lector de Códigos de Barras";
             this.TopMost = true;
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_lexico)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_sintactico)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -190,5 +318,14 @@
         private System.Windows.Forms.Label lbl_codehistory;
         private System.Windows.Forms.Button cmd_exit;
         private System.Windows.Forms.Button cmd_clean;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dgv_lexico;
+        private System.Windows.Forms.Label lbl_tabladetokens;
+        private System.Windows.Forms.Label lbl_validación;
+        private System.Windows.Forms.DataGridView dgv_sintactico;
+        private System.Windows.Forms.Label lbl_estructura;
+        private System.Windows.Forms.Label lbl_elementos;
     }
 }
--- LectorBarcodes/AddBarcodes.cs
+++ LectorBarcodes/AddBarcodes.cs
@@ -131,7 +131,7 @@ namespace LectorBarcodes
                         BarcodeFormat.UPC_E
                     },
                             PureBarcode = false,
-                            AssumeGS1 = true, 
+                            AssumeGS1 = true,
                             ReturnCodabarStartEnd = true
                         },
                         AutoRotate = true, // A veces ayuda activarlo
@@ -159,33 +159,38 @@ namespace LectorBarcodes
 
                         this.Invoke(new MethodInvoker(() =>
                         {
-                            if (result.Text.ToString().Length==13)
+                            string codigo = result.Text.ToString();
+
+                            // Comprobar si el código tiene longitud válida y agregar 0 al inicio si tiene 12 caracteres
+                            if (codigo.Length == 12 || codigo.Length == 13)
                             {
-                                txt_resultado.Text = result.Text;
-                                lbl_codehistory.Text = lbl_codehistory.Text + result.Text + "\n";
+                                // Si tiene 12 caracteres, agregar un 0 al inicio
+                                if (codigo.Length == 12)
+                                {
+                                    codigo = "0" + codigo;
+                                }
+
+                                // Actualizar los elementos de la interfaz
+                                txt_resultado.Text = codigo;
+                                lbl_codehistory.Text = lbl_codehistory.Text + codigo + "\n";
                                 lbl_status.Text = "¡Código detectado!";
                                 lbl_status.ForeColor = Color.Blue;
                                 System.Media.SystemSounds.Beep.Play();
                                 UpdatePreviewImage(frameToProcess);
-                            }
-                            else if (result.Text.ToString().Length == 12)
-                            {
-                                txt_resultado.Text = "0" + result.Text;
-                                lbl_codehistory.Text = lbl_codehistory.Text + "0" + result.Text + "\n";
-                                lbl_status.Text = "¡Código detectado!";
-                                lbl_status.ForeColor = Color.Blue;
-                                System.Media.SystemSounds.Beep.Play();
-                                UpdatePreviewImage(frameToProcess);
+
+                                // Mostrar tokens léxicos en el DataGridView optimizado
+                                MostrarTokensLexicos(codigo);
+
                             }
                             else
                             {
                                 lbl_status.Text = "¡Código dañado o incompleto!";
                                 lbl_status.ForeColor = Color.Red;
+                                dgv_lexico.Visible = false; // Ocultar el DataGridView si el código es inválido
                             }
 
                             //Lineas para el test de longitud de los codigos.
                             //MessageBox.Show("Longitud del texto detectado: " + result.Text.Length.ToString(), "Longitud");
-
                         }));
                     }
                     else
@@ -194,6 +199,9 @@ namespace LectorBarcodes
                         {
                             lbl_status.Text = "No se detectó ningún código.";
                             lbl_status.ForeColor = Color.Red;
+                            dgv_lexico.Visible = false; // Ocultar el DataGridView cuando no se detecta código
+                            lbl_validación.Visible = false;
+                            lbl_tabladetokens.Visible = false;
                         }));
                         frameToProcess.Dispose();
                     }
@@ -202,14 +210,132 @@ namespace LectorBarcodes
                 {
                     this.Invoke(new MethodInvoker(() =>
                     {
-                        lbl_status.Text = "Error al procesar: " + ex.Message;
+                        lbl_status.Text = "Error al invocar el método, intente de nuevo";
                         lbl_status.ForeColor = Color.Red;
+                        dgv_lexico.Visible = false; // Ocultar el DataGridView en caso de error
                     }));
                     frameToProcess.Dispose();
                 }
             });
         }
 
+        // Método separado para mostrar los tokens léxicos de manera optimizada
+        private void MostrarTokensLexicos(string codigo)
+        {
+            // Limpiar el DataGridView
+            dgv_lexico.Columns.Clear();
+            dgv_lexico.Rows.Clear();
+
+            // Configurar propiedades para reducir espacio
+            dgv_lexico.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dgv_lexico.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgv_lexico.RowHeadersVisible = false;  // Ocultar encabezados de filas
+
+            // Añadir columnas con nombres más cortos
+            for (int i = 0; i < codigo.Length; i++)
+            {
+                DataGridViewColumn col = new DataGridViewTextBoxColumn();
+                col.Name = $"col{i}";
+                col.HeaderText = $"T{i + 1}";  // "T1", "T2", etc.
+                col.Width = 30;  // Ancho fijo reducido
+                dgv_lexico.Columns.Add(col);
+            }
+
+            // Añadir los caracteres como fila
+            object[] fila = codigo.Select(c => (object)c.ToString()).ToArray();
+            dgv_lexico.Rows.Add(fila);
+
+            // Hacer visible el DataGridView
+            lbl_tabladetokens.Visible = true;
+            dgv_lexico.Visible = true;
+
+            //Validación
+            lbl_validación.Text = "Longitud válida: Sí (13 dígitos)\r\nTodos los caracteres son numéricos: ✔️";
+            lbl_validación.Visible = true;
+
+            RealizarAnalisisSintactico(codigo);
+        }
+
+        private void RealizarAnalisisSintactico(string codigo)
+        {
+            // Verificar que sea un código EAN-13 válido (13 dígitos)
+            if (codigo.Length != 13 || !codigo.All(char.IsDigit))
+            {
+                lbl_status.Text = "El código no cumple con el formato EAN-13 (13 dígitos numéricos)";
+                lbl_status.ForeColor = Color.Red;
+
+                // También mostrar mensaje de error en lbl_estructura
+                lbl_estructura.Text = "ERROR: El código no cumple con el formato EAN-13 (13 dígitos numéricos)";
+                lbl_estructura.ForeColor = Color.Red;
+                return;
+            }
+
+            // Extraer los componentes del código EAN-13
+            string codigoSistema = codigo.Substring(0, 1);
+            string codigoFabricante = codigo.Substring(1, 6);
+            string codigoProducto = codigo.Substring(7, 5);
+            string digitoControl = codigo.Substring(12, 1);
+
+            // Verificar el dígito de control
+            bool digitoControlValido = VerificarDigitoControl(codigo);
+
+            // Crear DataTable para mostrar los componentes
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Componente", typeof(string));
+            dt.Columns.Add("Valor", typeof(string));
+            dt.Columns.Add("Descripción", typeof(string));
+
+            // Agregar filas con los componentes
+            dt.Rows.Add("Sistema", codigoSistema, "Código del sistema");
+            dt.Rows.Add("Fabricante", codigoFabricante, "Código del fabricante");
+            dt.Rows.Add("Producto", codigoProducto, "Código del producto");
+            dt.Rows.Add("Control", digitoControl, "Dígito de verificación");
+            dt.Rows.Add("Validación", digitoControlValido ? "✓ Válido" : "✗ Inválido",
+                digitoControlValido ? "El dígito de control es correcto" : "El dígito de control no coincide");
+
+            // Mostrar en el DataGridView
+            dgv_sintactico.Visible = true;
+            dgv_sintactico.DataSource = dt;
+
+            // Formatear y mostrar el mensaje según sea válido o no
+            string estructuraMsg = $"Estructura: [{codigoSistema}] Código del sistema - [{codigoFabricante}] Fabricante " +
+                                  $"[{codigoProducto}] Producto - [{digitoControl}] Dígito de control";
+
+            if (digitoControlValido)
+            {
+                lbl_elementos.Text = estructuraMsg;
+                lbl_elementos.ForeColor = Color.Black;
+                lbl_estructura.Text = "Formato EAN-13 válido: ✓";
+                lbl_estructura.ForeColor = Color.Green; // Color verde para mensaje positivo
+            }
+            else
+            {
+                lbl_elementos.ForeColor = Color.Black;
+                lbl_elementos.Text = estructuraMsg;
+                lbl_estructura.Text = "Formato EAN-13 inválido: ✗ - El dígito de control no coincide";
+                lbl_estructura.ForeColor = Color.Red; // Color rojo para mensaje negativo
+            }
+            //Mostramos los resultados
+            lbl_elementos.Visible = true;
+            lbl_estructura.Visible = true;
+        }
+
+        // Método para verificar el dígito de control de un código EAN-13
+        private bool VerificarDigitoControl(string codigo)
+        {
+            if (codigo.Length != 13 || !codigo.All(char.IsDigit))
+                return false;
+            // Algoritmo para calcular el dígito de control EAN-13
+            int suma = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                int digito = int.Parse(codigo[i].ToString());
+                suma += (i % 2 == 0) ? digito : digito * 3;
+            }
+            int digitoCalculado = (10 - (suma % 10)) % 10;
+            int digitoReal = int.Parse(codigo[12].ToString());
+            return digitoCalculado == digitoReal;
+        }
 
         private VideoCapabilities SelectOptimalResolution(VideoCaptureDevice device)
         {
@@ -341,6 +467,15 @@ namespace LectorBarcodes
 
         private void AddBarcodes_Load(object sender, EventArgs e)
         {
+            //Elementos de los analisis
+            dgv_lexico.Visible = false;
+            dgv_sintactico.Visible = false;
+            lbl_validación.Visible = false;
+            lbl_tabladetokens.Visible = false;
+            lbl_estructura.Visible = false;
+            lbl_elementos.Visible = false;
+
+
             // Crea un label para mostrar el estado si no existe
             if (lbl_status == null)
             {
@@ -399,8 +534,14 @@ namespace LectorBarcodes
         private void button1_Click(object sender, EventArgs e)
         {
             lbl_codehistory.Text = "";
-            txt_resultado.Text = "";   
+            txt_resultado.Text = "";
+            lbl_elementos.Text = "";
+            lbl_estructura.Text = "";
             lbl_status.Text = "Historial de códigos borrado";
+            dgv_lexico.Visible = false;
+            lbl_tabladetokens.Visible = false;
+            lbl_validación.Visible = false;
+            
         }
     }
 }
